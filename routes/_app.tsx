eea601a676ec920a898496b6416a1018cca8d2fb
Berry de Vos <bdevos@gmail.com>
2023-10-22 09:50:28 +0200

    Commit before running fresh update

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,9 +1,55 @@
 import { AppProps } from "$fresh/server.ts";
+import { categories, Category } from "../categories.ts";
+import { Icon } from "../icons/Icon.tsx";
 
 export default function App({ Component }: AppProps) {
   return (
-    <body class="bg-gray-100 dark:bg-black">
+    <body className="bg-gray-100 dark:bg-black">
       <Component />
+
+      <dialog
+        id="settingsDialog"
+        className="mt-6 bg-gray-100 w-screen backdrop:backdrop-blur-sm sm:bg-gray-100 sm:w-[640px]"
+      >
+        <form method="post" action=".">
+          <fieldset>
+            <legend className="text-base font-semibold leading-6 text-gray-900">
+              Categories
+            </legend>
+            <div className="mt-4 divide-y divide-gray-200 border-b border-t border-gray-200">
+              {Object.entries(categories).map(([category, label]) => (
+                <div key={category} className="relative flex items-start py-0">
+                  <div className="min-w-0 flex-1 text-sm leading-6">
+                    <label
+                      htmlFor={`check-${category}`}
+                      className="select-none font-medium text-gray-900"
+                    >
+                      <Icon category={category as Category} />
+                      <span>{label}</span>
+                    </label>
+                  </div>
+                  <div className="ml-3 flex items-center">
+                    <input
+                      id={`check-${category}`}
+                      name={`check-${category}`}
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </fieldset>
+          <button value="cancel" formMethod="dialog">Cancel</button>
+          <button
+            type="submit"
+            autoFocus
+            className="rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+          >
+            Save
+          </button>
+        </form>
+      </dialog>
     </body>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,97 @@
 import { Head } from "$fresh/runtime.ts";
+import { categories, Category } from "../categories.ts";
+import { Icon } from "../icons/Icon.tsx";
+import Updated from "../icons/Updated.tsx";
+import { SettingsButton } from "../islands/SettingsButton.tsx";
+
+type Link = {
+  category: Category;
+  title: string;
+  id: string;
+  url: string;
+  published: Date;
+  updated: Date;
+};
+
+const links: Link[] = [{
+  category: "apple",
+  title: "iFixit tears down… Apple’s FineWoven cases",
+  url:
+    "https://www.theverge.com/2023/9/22/23886030/apple-finewoven-cases-ifixit-teardown",
+  id:
+    "https://www.theverge.com/2023/9/22/23886030/apple-finewoven-cases-ifixit-teardown",
+  published: new Date(),
+  updated: new Date(),
+}, {
+  category: "climate-change",
+  title: "Apple iPhone 15 Pro and Pro Max review: by the numbers",
+  url: "https://www.theverge.com/23879619/apple-iphone-15-pro-max-review",
+  id: "https://www.theverge.com/23879619/apple-iphone-15-pro-max-review",
+  published: new Date(),
+  updated: new Date(),
+}, {
+  category: "facebook",
+  title: "Hulu’s No One Will Save You is taut, minimalist sci-fi horror",
+  url: "https://www.theverge.com/23883971/no-one-will-save-you-review-hulu",
+  id: "https://www.theverge.com/23883971/no-one-will-save-you-review-hulu",
+  published: new Date(),
+  updated: new Date(),
+}];
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>¹</title>
+        <title>theshook.¹</title>
       </Head>
-      <div class="min-h-[100svh] grid grid-cols-1 place-content-center place-items-center">
-        <div class="text-9xl">
-          🫨
+      <div class="mx-auto max-w-4xl px-2 sm:px-6 lg:px-8">
+        <div class="flex h-12 justify-between items-center">
+          <div class="text-8xl">🫨</div>
+          <div>
+            <SettingsButton />
+          </div>
+        </div>
+        <ul className="mt-8">
+          {links.map(({ id, url, title, category }) => (
+            <li key={id} className="">
+              <a
+                href={url}
+                target="_blank"
+                className="flex flex-row text-[0.75rem] gap-2 whitespace-nowrap items-baseline"
+              >
+                <div className="font-mono text-[0.55rem]">
+                  10:00
+                </div>
+                <div className="grow truncate font-sans">
+                  {title}
+                </div>
+                <div className="text-[0.9rem] h-2">
+                  <Icon category={category} />
+                </div>
+              </a>
+            </li>
+          ))}
+          <li className="relative">
+            <div
+              className="absolute inset-0 flex items-center"
+              aria-hidden="true"
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <h3 className="relative flex justify-start">
+              <span className="font-mono bg-gray-100 ml-2 px-1 text-[0.7rem] font-bold leading-6 text-gray-900">
+                YESTERDAY
+              </span>
+            </h3>
+          </li>
+          <li>
+          </li>
+          <li>
+            <h3 className="font-mono text-[0.6rem] font-bold">SEP 21</h3>
+          </li>
+        </ul>
+        <div class="flex font-mono text-[0.9rem] text-gray-300 gap-1">
+          <Updated /> just now
         </div>
       </div>
     </>
