83026a5c1c5a6dd9e66711cd6a38238c7d7ae6f3
Berry de Vos <bdevos@gmail.com>
2023-11-19 16:51:16 +0100

    Disable the github update and update deno cron to use every hour

--- main.ts
+++ main.ts
@@ -12,6 +12,6 @@ import twindPlugin from '$fresh/plugins/twindv1.ts'
 import twindConfig from './twind.config.ts'
 import { updateEntries } from './src/kv/update.ts'
 
-Deno.cron('Update entries', '*/10 * * * *', () => updateEntries())
+Deno.cron('Update entries', '0 * * * *', () => updateEntries())
 
 await start(manifest, { plugins: [twindPlugin(twindConfig)] })
