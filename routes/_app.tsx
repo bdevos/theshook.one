40f21f3ef6a5a741907d79a4dd0305ebdfd820c8
Berry de Vos <bdevos@gmail.com>
2024-05-04 07:46:31 +0200

    Load stylesheet through asset function so it is hashed and cached

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,5 @@
 import { PageProps } from '$fresh/server.ts'
-import { Head } from '$fresh/runtime.ts'
+import { asset, Head } from '$fresh/runtime.ts'
 
 export default function App({ Component }: PageProps) {
   return (
@@ -7,7 +7,7 @@ export default function App({ Component }: PageProps) {
       <Head>
         <link rel='icon' type='image/x-icon' href='/favicon.ico' />
         <link rel='apple-touch-icon' href='/3311.png' />
-        <link rel='stylesheet' href='/styles.css' />
+        <link rel='stylesheet' href={asset('/styles.css')} />
       </Head>
       <body class='bg-neutral-50 dark:bg-black text-neutral-900 dark:text-neutral-50'>
         <Component />
